c24f94eb1460728a5a0b7c481befc7976fa23777
arash-oyesters <arash@oyesters.in>
2023-02-13 18:02:10 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,172 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react";
+import * as XLSX from "xlsx";
+import "./pages/Table.css";
+import Table2 from "./pages/TableTwo";
+import { useReactToPrint } from "react-to-print";
 
-function App() {
+const App = () => {
+    const componentToPrintRef = useRef();
+   const handlePrint = useReactToPrint({
+     content: () => componentToPrintRef.current,
+   });
+  const fileUploadRef = useRef(null);
+  const [heading, setHeading] = useState([]);
+  const [selectedCat, setSelectedCat] = useState("");
+  const [items, setItems] = useState([]);
+  const [selectedItems, setSelectedItems] = useState([]);
+  const [data, setData] = useState(null);
+
+  console.log("test :", items[0]?.[selectedCat]);
+
+  const catIdtoValue = (catId) => {
+    if (catId === "") {
+      return "";
+    }
+    if (catId === "SC") {
+      return 3;
+    }
+    if (catId === "ST") {
+      return 4;
+    }
+    if (catId === "VJ") {
+      return 5;
+    }
+    if (catId === "NT1") {
+      return 6;
+    }
+    if (catId === "NT2") {
+      return 7;
+    }
+    if (catId === "NT3") {
+      return 8;
+    }
+    if (catId === "OBC") {
+      return 9;
+    }
+    if (catId === "EWS") {
+      return 10;
+    }
+    if (catId === "OPEN") {
+      return 11;
+    }
+  };
+
+
+  useEffect(() => {
+    if (items.length !== 0 && selectedCat !== "") {
+      setHeading([
+        Object.keys(items[0])[0],
+        Object.keys(items[0])[1],
+        Object.keys(items[0])[2],
+        Object.keys(items[0])[catIdtoValue(selectedCat)],
+      ]);
+      console.log("heading:", heading);
+    }
+    if (items.length !== 0 && selectedCat === "") {
+      console.log("heading:", heading);
+      setHeading(Object.keys(items[0]));
+    }
+  }, [items, selectedCat]);
+
+  const readExcel = (file) => {
+    const promise = new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.readAsArrayBuffer(file);
+
+      fileReader.onload = (e) => {
+        const bufferArray = e.target.result;
+
+        const wb = XLSX.read(bufferArray, { type: "buffer" });
+
+        const wsname = wb.SheetNames[0];
+
+        const ws = wb.Sheets[wsname];
+
+        const data = XLSX.utils.sheet_to_json(ws);
+
+        resolve(data);
+      };
+
+      fileReader.onerror = (error) => {
+        reject(error);
+      };
+    });
+
+    promise.then((d) => {
+      setItems(d);
+    });
+  };
+
+  const handleShift = (table, index) => {
+    if (table) {
+      let data = items[index];
+      if (data) {
+        items.splice(index, 1);
+        setSelectedItems([...selectedItems, data]);
+      }
+    } else {
+      let data = selectedItems[index];
+      if (data) {
+        selectedItems.splice(index, 1);
+        setItems([...items, data]);
+      }
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      {items.length === 0 && selectedItems.length === 0 ? (
+        <div className="input_container">
+          <div className="input_box">
+            <h2>Upload Your Excel File</h2>
+            <div>
+              <input
+                type="file"
+                ref={fileUploadRef}
+                onChange={(e) => {
+                  setData(e.target.files[0]);
+                  readExcel(e.target.files[0]);
+                }}
+                accept=".xlsx"
+                style={{
+                  visibility: "hidden",
+                }}
+              />
+              <div className="browse-files">
+                <button
+                  class="button-28"
+                  role="button"
+                  onClick={() => fileUploadRef.current.click()}
+                >
+                  Upload
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <>
+          <div className="table_container" ref={componentToPrintRef}>
+            <Table2
+              selectedCat={selectedCat}
+              setSelectedCat={setSelectedCat}
+              heading={heading}
+              items={items}
+              handleShift={handleShift}
+            />
+          </div>
+          <button
+            style={{ maxWidth: "200px" }}
+            class="button-28 print"
+            role="button "
+            onClick={handlePrint}
+          >
+            Download as PDF
+          </button>
+        </>
+      )}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/pages/TableTwo.jsx
@@ -0,0 +1,190 @@
+import React, { useState } from "react";
+import jsPDF from "jspdf";
+import "jspdf-autotable";
+
+const Table2 = ({
+  heading,
+  items,
+  handleShift,
+  setSelectedCat,
+  selectedCat,
+}) => {
+  const [rank, setRank] = useState(null);
+  // const [selectedItems, setSelectedItems] = useState([]);
+
+  // const handleCheckboxChange = (item, index) => {
+  //   let updatedSelectedItems = [...selectedItems];
+  //   if (selectedItems.includes(index)) {
+  //     updatedSelectedItems = selectedItems.filter(
+  //       (selectedIndex) => selectedIndex !== index
+  //     );
+  //   } else {
+  //     updatedSelectedItems.push(index);
+  //   }
+  //   setSelectedItems(updatedSelectedItems);
+  // };
+
+  // const handlePrint = () => {
+  //   const selectedData = selectedItems.map((index) => items[index]);
+  //   const headers = Object.keys(selectedData[0]);
+  //   const convertedArray = selectedData.map((obj) =>
+  //     headers.map((header) => obj[header])
+  //   );
+
+  //   const doc = new jsPDF("p", "pt");
+
+  //   const columns = heading.map((h) => {
+  //     return { title: h, dataKey: h };
+  //   });
+
+  //   doc.autoTable({
+  //     head: [columns],
+  //     body: convertedArray,
+  //     styles: {
+  //       fontSize: 8,
+  //     },
+  //   });
+
+  //   doc.save("selected-data.pdf");
+  // };
+
+  return (
+    <div className="table_box">
+      {/* input */}
+      <div
+        class="row"
+        style={{ display: "flex", justifyContent: "space-between" }}
+      >
+        <span>
+          <label class="slide1" for="name">
+            Name
+          </label>
+          <input class="basic-slide" id="name" type="text" placeholder="Name" />
+        </span>
+        <span>
+          <label class="slide2" for="caste">
+            Caste
+          </label>
+          <select
+            id="category"
+            name="category"
+            value={selectedCat}
+            onChange={(e) => setSelectedCat(e.target.value)}
+          >
+            <option value="">All</option>
+            <option value="SC">SC</option>
+            <option value="ST">ST</option>
+            <option value="VJ">VJ</option>
+            <option value="NT1">NT1</option>
+            <option value="NT2">NT2</option>
+            <option value="NT3">NT3</option>
+            <option value="OBC">OBC</option>
+            <option value="EWS">EWS</option>
+            <option value="OPEN">OPEN</option>
+          </select>
+        </span>
+        <span>
+          <label class="slide3" for="phone">
+            Rank
+          </label>
+          <input
+            class="basic-slide"
+            id="phone"
+            type="number"
+            placeholder="Your Rank"
+            value={rank}
+            onChange={(e) => setRank(parseInt(e.target.value))}
+          />
+        </span>
+      </div>
+
+      {/* input end  */}
+
+
+      {rank && <h3>College less than Rank : {rank}</h3>}
+      <table
+        // className="table"
+        class="styled-table"
+      >
+        <thead>
+          <tr>
+            {heading.map((h) => (
+              <th scope="col" className="table_heading">
+                {h === selectedCat ? "Rank" : h}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        
+        <tbody>
+          {items
+            .filter((item) => {
+              if (selectedCat !== "") {
+                if (rank === undefined || rank === null) {
+                  return item;
+                } else {
+                  return item?.[selectedCat] <= rank;
+                }
+              } else {
+                return item;
+              }
+            })
+            .map((d, idx) => (
+              <tr key={idx}>
+                {heading.map((h) => {
+                  return (
+                    <td align="center" className="table_data">
+                      {d[h]}
+                    </td>
+                  );
+                })}
+              </tr>
+            ))}
+        </tbody>
+      </table>
+
+      {rank && (
+        <>
+          <h3>College greater than Rank : {rank}</h3>
+          <table
+            // className="table"
+            class="styled-table"
+          >
+            <thead>
+              <tr>
+                {heading.map((h) => (
+                  <th scope="col" className="table_heading">
+                    {h === selectedCat ? "Rank" : h}
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {items
+                .filter((item) => {
+                  if (selectedCat !== "") {
+                    if (rank === undefined || rank === null) {
+                      return item;
+                    } else {
+                      return item?.[selectedCat] > rank;
+                    }
+                  } else {
+                    return item;
+                  }
+                })
+                .map((d, idx) => (
+                  <tr key={idx}>
+                    {heading.map((h) => {
+                      return <td align="left">{d[h]}</td>;
+                    })}
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Table2;
